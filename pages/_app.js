7f49df5e13114a56c5ac591be6ab48e3b2e910d9
stanley-yin <stanley6140992@gmail.com>
2023-06-24 17:19:15 +0800

    feat: add gtm script

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,10 @@
 import "../styles/global.css";
+import { useEffect } from "react";
+import TagManager from "react-gtm-module";
 
 export default function App({ Component, pageProps }) {
+  useEffect(() => {
+    TagManager.initialize({ gtmId: "GTM-W2QVHK3" });
+  }, []);
   return <Component {...pageProps} />;
 }
